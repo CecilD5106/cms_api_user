9ad4f2d773e2788959118054c5abe094a36e3d40
Cecil Davis <cecild5106@hotmail.com>
2020-06-28 10:13:23 -0500

    CRUD functions complete

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -19,7 +20,7 @@ func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "cgdavis"
 	dbPass := "DzftXvz$eR7VpY^h"
-	dbServer := "tcp(172.18.105.227:3306)"
+	dbServer := "tcp(172.17.232.252:3306)"
 	dbName := "people"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbServer+"/"+dbName)
 	if err != nil {
@@ -31,7 +32,7 @@ func dbConn() (db *sql.DB) {
 // GetPeople returns all records in the person table
 func GetPeople(c *gin.Context) {
 	db := dbConn()
-	selDB, err := db.Query("SELECT * FROM person")
+	selDB, err := db.Query("CALL read_all_people()")
 	if err != nil {
 		panic(err.Error)
 	}
@@ -60,105 +61,88 @@ func GetPeople(c *gin.Context) {
 	defer db.Close()
 }
 
-/*
-func Show(w http.ResponseWriter, r *http.Request) {
+// GetPerson returns a single person record from the database
+func GetPerson(c *gin.Context) {
+	nID := c.Param("person_id")
 	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM person WHERE person_id=?", nId)
+	selDB, err := db.Query("CALL read_person(?)", nID)
 	if err != nil {
-		panic(err.Error())
+		panic(err.Error)
 	}
+
 	indiv := Person{}
+	people := []Person{}
 	for selDB.Next() {
 		var id, fname, lname string
 		err = selDB.Scan(&id, &fname, &lname)
 		if err != nil {
-			panic(err.Error())
+			log.Println(err)
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
 		}
 		indiv.ID = id
 		indiv.FName = fname
 		indiv.LName = lname
+		people = append(people, indiv)
 	}
-	tmpl.ExecuteTemplate(w, "Show", indiv)
+
+	c.JSON(200, gin.H{
+		"result": people,
+	})
+
 	defer db.Close()
 }
 
-func New(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "New", nil)
-}
-
-func Edit(w http.ResponseWriter, r *http.Request) {
+// CreatePerson adds person information to the database
+func CreatePerson(c *gin.Context) {
 	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM person WHERE person_id=?", nId)
-	if err != nil {
-		panic(err.Error())
-	}
-	indiv := Person{}
-	for selDB.Next() {
-		var id, fname, lname string
-		err = selDB.Scan(&id, &fname, &lname)
+	var person Person
+	if err := c.BindJSON(&person); err == nil {
+		statement, _ := db.Prepare("CALL create_person (?, ?)")
+		statement.Exec(person.FName, person.LName)
 		if err != nil {
-			panic(err.Error())
+			c.String(http.StatusInternalServerError, err.Error())
 		}
-		indiv.ID = id
-		indiv.FName = fname
-		indiv.LName = lname
+	} else {
+		c.String(http.StatusInternalServerError, err.Error())
 	}
-	tmpl.ExecuteTemplate(w, "Edit", indiv)
 	defer db.Close()
 }
 
-func Insert(w http.ResponseWriter, r *http.Request) {
+// UpdatePerson updates information in the person record
+func UpdatePerson(c *gin.Context) {
 	db := dbConn()
-	if r.Method == "POST" {
-		fname := r.FormValue("fname")
-		lname := r.FormValue("lname")
-		insForm, err := db.Prepare("CALL insert_person(?, ?)")
+	var person Person
+	if err := c.BindJSON(&person); err == nil {
+		statement, _ := db.Prepare("CALL update_person (?, ?, ?)")
+		statement.Exec(person.ID, person.FName, person.LName)
 		if err != nil {
-			panic(err.Error())
+			c.String(http.StatusInternalServerError, err.Error())
 		}
-		insForm.Exec(fname, lname)
-		log.Println("INSERT: First Name: " + fname + " | Last Name: " + lname)
+	} else {
+		c.String(http.StatusInternalServerError, err.Error())
 	}
 	defer db.Close()
-	http.Redirect(w, r, "/", 301)
 }
 
-func Update(w http.ResponseWriter, r *http.Request) {
+// DeletePerson deletes a person record from the database
+func DeletePerson(c *gin.Context) {
+	nID := c.Param("person_id")
 	db := dbConn()
-	if r.Method == "POST" {
-		fname := r.FormValue("fname")
-		lname := r.FormValue("lname")
-		id := r.FormValue("uid")
-		insForm, err := db.Prepare("UPDATE person SET first_name=?, last_name=? WHERE person_id=?")
-		if err != nil {
-			panic(err.Error())
-		}
-		insForm.Exec(fname, lname, id)
-		log.Println("UPDATE: First Name: " + fname + " | Last Name: " + lname)
-	}
+	statement, _ := db.Prepare("CALL delete_person(?)")
+	statement.Exec(nID)
 	defer db.Close()
-	http.Redirect(w, r, "/", 301)
 }
 
-func Delete(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	indiv := r.URL.Query().Get("id")
-	delForm, err := db.Prepare("DELETE FROM person WHERE person_id=?")
-	if err != nil {
-		panic(err.Error())
-	}
-	delForm.Exec(indiv)
-	log.Println("DELETE")
-	defer db.Close()
-	http.Redirect(w, r, "/", 301)
-}
-*/
 func main() {
 	r := gin.Default()
 
-	r.GET("/", GetPeople)
+	r.GET("/getpeople", GetPeople)
+	r.GET("/getperson/:person_id", GetPerson)
+	r.GET("/deleteperson/:person_id", DeletePerson)
+	r.POST("/createperson", CreatePerson)
+	r.POST("/updateperson", UpdatePerson)
 
 	r.Run(":8000")
 }
