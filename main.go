48004f69a8f62071a0b0393a976bb607e2a6d703
Cecil Davis <cecild5106@hotmail.com>
2020-06-28 21:06:51 -0500

    CMS User Update V1

--- main.go
+++ main.go
@@ -9,11 +9,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Person is the basic structure of a person record
-type Person struct {
-	ID    string `json:"person_id"`
-	FName string `json:"first_name"`
-	LName string `json:"last_name"`
+// User is the basic structure of a user record
+type User struct {
+	ID    			string	`json:"user_id"`
+	UserName		string	`json:"user_name"`
+	FName 			string	`json:"user_first_name"`
+	LName 			string	`json:"user_last_name"`
+	Password		string	`json:"password"`
+	PasswordChange	string	`json:"password_change"`
+	PasswordExpired	string	`json:"password_expired"`
+	LastLogon		string	`json:"last_logon"`
+	AccountLocked	string	`json:"account_locked"`
 }
 
 func dbConn() (db *sql.DB) {
@@ -21,7 +27,7 @@ func dbConn() (db *sql.DB) {
 	dbUser := "cgdavis"
 	dbPass := "DzftXvz$eR7VpY^h"
 	dbServer := "tcp(172.17.232.252:3306)"
-	dbName := "people"
+	dbName := "cms"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbServer+"/"+dbName)
 	if err != nil {
 		panic(err.Error())
@@ -29,78 +35,90 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-// GetPeople returns all records in the person table
-func GetPeople(c *gin.Context) {
+// GetUsers returns all records in the user table
+func GetUsers(c *gin.Context) {
 	db := dbConn()
-	selDB, err := db.Query("CALL read_all_people()")
+	selDB, err := db.Query("CALL read_users()")
 	if err != nil {
 		panic(err.Error)
 	}
 
-	person := Person{}
-	people := []Person{}
+	user := User{}
+	users := []User{}
 	for selDB.Next() {
-		var id, fname, lname string
-		err = selDB.Scan(&id, &fname, &lname)
+		var id, username, fname, lname, password, passwordchange, passwordexpired, lastlogon, accountlocked string
+		err = selDB.Scan(&id, &username, &fname, &lname, &password, &passwordchange, &passwordexpired, &lastlogon, &accountlocked)
 		if err != nil {
 			log.Println(err)
 			c.JSON(500, gin.H{
 				"error": err.Error(),
 			})
 		}
-		person.ID = id
-		person.FName = fname
-		person.LName = lname
-		people = append(people, person)
+		user.ID = id
+		user.UserName = username
+		user.FName = fname
+		user.LName = lname
+		user.Password = password
+		user.PasswordChange = passwordchange
+		user.PasswordExpired = passwordexpired
+		user.LastLogon = lastlogon
+		user.AccountLocked = accountlocked
+		users = append(users, user)
 	}
 
 	c.JSON(200, gin.H{
-		"result": people,
+		"result": users,
 	})
 
 	defer db.Close()
 }
 
-// GetPerson returns a single person record from the database
-func GetPerson(c *gin.Context) {
-	nID := c.Param("person_id")
+// GetUser returns a single user record from the database
+func GetUser(c *gin.Context) {
+	nID := c.Param("user_id")
 	db := dbConn()
-	selDB, err := db.Query("CALL read_person(?)", nID)
+	selDB, err := db.Query("CALL read_user(?)", nID)
 	if err != nil {
 		panic(err.Error)
 	}
 
-	indiv := Person{}
-	people := []Person{}
+	user := User{}
+	users := []User{}
 	for selDB.Next() {
-		var id, fname, lname string
-		err = selDB.Scan(&id, &fname, &lname)
+		var id, username, fname, lname, password, passwordchange, passwordexpired, lastlogon, accountlocked string
+		err = selDB.Scan(&id, &username, &fname, &lname, &password, &passwordchange, &passwordexpired, &lastlogon, &accountlocked)
 		if err != nil {
 			log.Println(err)
 			c.JSON(500, gin.H{
 				"error": err.Error(),
 			})
 		}
-		indiv.ID = id
-		indiv.FName = fname
-		indiv.LName = lname
-		people = append(people, indiv)
+		user.ID = id
+		user.UserName = username
+		user.FName = fname
+		user.LName = lname
+		user.Password = password
+		user.PasswordChange = passwordchange
+		user.PasswordExpired = passwordexpired
+		user.LastLogon = lastlogon
+		user.AccountLocked = accountlocked
+		users = append(users, user)
 	}
 
 	c.JSON(200, gin.H{
-		"result": people,
+		"result": users,
 	})
 
 	defer db.Close()
 }
 
-// CreatePerson adds person information to the database
-func CreatePerson(c *gin.Context) {
+// CreateUser adds user information to the database
+func CreateUser(c *gin.Context) {
 	db := dbConn()
-	var person Person
-	if err := c.BindJSON(&person); err == nil {
-		statement, _ := db.Prepare("CALL create_person (?, ?)")
-		statement.Exec(person.FName, person.LName)
+	var user User
+	if err := c.BindJSON(&user); err == nil {
+		statement, _ := db.Prepare("CALL create_user (?, ?, ?, ?, ?, ?, ?, ?)")
+		statement.Exec(user.UserName, user.FName, user.LName, user.Password, user.PasswordChange, user.PasswordExpired, user.LastLogon, user.AccountLocked)
 		if err != nil {
 			c.String(http.StatusInternalServerError, err.Error())
 		}
@@ -110,13 +128,13 @@ func CreatePerson(c *gin.Context) {
 	defer db.Close()
 }
 
-// UpdatePerson updates information in the person record
-func UpdatePerson(c *gin.Context) {
+// UpdateUser updates information in the user record
+func UpdateUser(c *gin.Context) {
 	db := dbConn()
-	var person Person
-	if err := c.BindJSON(&person); err == nil {
-		statement, _ := db.Prepare("CALL update_person (?, ?, ?)")
-		statement.Exec(person.ID, person.FName, person.LName)
+	var user User
+	if err := c.BindJSON(&user); err == nil {
+		statement, _ := db.Prepare("CALL update_user (?, ?, ?, ?, ?, ?, ?, ?, ?)")
+		statement.Exec(user.ID, user.UserName, user.FName, user.LName, user.Password, user.PasswordChange, user.PasswordExpired, user.LastLogon, user.AccountLocked)
 		if err != nil {
 			c.String(http.StatusInternalServerError, err.Error())
 		}
@@ -126,11 +144,11 @@ func UpdatePerson(c *gin.Context) {
 	defer db.Close()
 }
 
-// DeletePerson deletes a person record from the database
-func DeletePerson(c *gin.Context) {
-	nID := c.Param("person_id")
+// DeleteUser deletes a user record from the database
+func DeleteUser(c *gin.Context) {
+	nID := c.Param("user_id")
 	db := dbConn()
-	statement, _ := db.Prepare("CALL delete_person(?)")
+	statement, _ := db.Prepare("CALL delete_user(?)")
 	statement.Exec(nID)
 	defer db.Close()
 }
@@ -138,11 +156,11 @@ func DeletePerson(c *gin.Context) {
 func main() {
 	r := gin.Default()
 
-	r.GET("/getpeople", GetPeople)
-	r.GET("/getperson/:person_id", GetPerson)
-	r.GET("/deleteperson/:person_id", DeletePerson)
-	r.POST("/createperson", CreatePerson)
-	r.POST("/updateperson", UpdatePerson)
+	r.GET("/v1/getusers", GetUsers)
+	r.GET("/v1/getuser/:user_id", GetUser)
+	r.GET("/v1/deleteuser/:user_id", DeleteUser)
+	r.POST("/v1/createuser", CreateUser)
+	r.POST("/v1/updateuser", UpdateUser)
 
 	r.Run(":8000")
 }
